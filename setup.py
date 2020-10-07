96dee67f772ae53e2c59071e26a943c495d644e1
joaopfonseca <jpmrfonseca@gmail.com>
2020-10-07 13:10:58 +0100

    Adapted setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name='fonseca_research',
     packages=find_packages(),
     version='0.1.0',
-    description='A short description of the project.',
+    description='Source code for my own research. Contains most of the source code (LaTeX, Python, etc.) of all papers I have been involved in.',
     author='joaopfonseca',
     license='MIT',
 )
