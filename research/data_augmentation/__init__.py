b996182a9886053cca0d23efc1c8f4f872312850
joaopfonseca <jpmrfonseca@gmail.com>
2021-07-13 16:46:01 +0100

    FEAT add modified G-SMOTE

---
+++ research/data_augmentation/__init__.py
@@ -0,0 +1,10 @@
+"""
+Contains the implementation of variations of oversampling/data augmentation
+algorithms, as well as helper classes to use oversampling algorithms as data
+augmentation techniques.
+"""
+
+from ._oversampling_augmentation import OverSamplingAugmentation
+from ._gsmote import GeometricSMOTE
+
+__all__ = ['OverSamplingAugmentation', 'GeometricSMOTE']
