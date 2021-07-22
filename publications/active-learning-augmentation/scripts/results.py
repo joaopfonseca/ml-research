9572b52f641698dbb6eb9524ddc174499dde85eb
joaopfonseca <jpmrfonseca@gmail.com>
2021-07-22 22:20:32 +0000

    FIX wrong import

--- publications/active-learning-augmentation/scripts/results.py
+++ publications/active-learning-augmentation/scripts/results.py
@@ -14,7 +14,7 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.neighbors.classification import KNeighborsClassifier
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.dummy import DummyClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import StratifiedKFold
