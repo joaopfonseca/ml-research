98cc72cb5410b1e9edd86f538de2a47c41bee444
joaopfonseca <jpmrfonseca@gmail.com>
2021-12-28 22:38:17 +0000

    MNT move versioning comments from __init__ to _version

--- research/__init__.py
+++ research/__init__.py
@@ -1,24 +1,7 @@
 """
 `research` is a library containing the source code for the algorithms
-used in my own research.
+used to conduct research in Machine Learning.
 """
 from ._version import __version__
-
-# PEP0440 compatible formatted version, see:
-# https://www.python.org/dev/peps/pep-0440/
-#
-# Generic release markers:
-#   X.Y
-#   X.Y.Z   # For bugfix releases
-#
-# Admissible pre-release markers:
-#   X.YaN   # Alpha release
-#   X.YbN   # Beta release
-#   X.YrcN  # Release Candidate
-#   X.Y     # Final release
-#
-# Dev branch marker is: 'X.Y.dev' or 'X.Y.devN' where N is an integer.
-# 'X.Y.dev0' is the canonical version of 'X.Y.dev'
-#
 
 __all__ = ["__version__"]
