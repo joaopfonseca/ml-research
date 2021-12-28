60dd1b00ee5e7da63c2091bd1d65b39ac68f03c5
joaopfonseca <jpmrfonseca@gmail.com>
2021-12-28 22:37:19 +0000

    bumpversion 0.3.5.dev0

--- research/_version.py
+++ research/_version.py
@@ -1,1 +1,23 @@
-__version__ = "0.3.4"
+"""
+`research` is a library containing the source code for the algorithms
+used to conduct research in Machine Learning.
+"""
+
+# PEP0440 compatible formatted version, see:
+# https://www.python.org/dev/peps/pep-0440/
+#
+# Generic release markers:
+#   X.Y
+#   X.Y.Z   # For bugfix releases
+#
+# Admissible pre-release markers:
+#   X.YaN   # Alpha release
+#   X.YbN   # Beta release
+#   X.YrcN  # Release Candidate
+#   X.Y     # Final release
+#
+# Dev branch marker is: 'X.Y.dev' or 'X.Y.devN' where N is an integer.
+# 'X.Y.dev0' is the canonical version of 'X.Y.dev'
+#
+
+__version__ = "0.3.5.dev0"
