dd09c9dcf78548fb60da0a3b9a58486fb18ed4d4
joaopfonseca <jpmrfonseca@gmail.com>
2022-03-18 17:37:02 +0000

    TEST add assertions and remove test of deprecated feature

--- mlresearch/self_supervised/tests/test_scheduler.py
+++ mlresearch/self_supervised/tests/test_scheduler.py
@@ -1,34 +1,32 @@
-import torch
 from torch import nn
 from torch.optim import Adam
 
-from .._byol import LinearWarmupCosineAnnealingLR
+from .._components import LinearWarmupCosineAnnealingLR
 
 
 def test_lr_decay():
-    scheduler1 = LinearWarmupCosineAnnealingLR(
-        Adam(nn.Linear(10, 1).parameters(), lr=0.02), warmup_epochs=10, max_epochs=40
+    start_lr = 0.001
+    last_lr = 0.0001
+    epochs = 40
+    scheduler = LinearWarmupCosineAnnealingLR(
+        Adam(nn.Linear(10, 1).parameters(), lr=0.02),
+        warmup_epochs=10,
+        max_epochs=epochs,
+        warmup_start_lr=start_lr,
+        eta_min=last_lr,
     )
-    scheduler2 = LinearWarmupCosineAnnealingLR(
-        Adam(nn.Linear(10, 1).parameters(), lr=0.02), warmup_epochs=10, max_epochs=40
-    )
-
     # the default case
     default_lr = []
-    for epoch in range(40):
-        scheduler1.step()
-        default_lr.append(scheduler1.get_last_lr())
-
-    # passing epoch param case
-    epoch_lr = []
-    for epoch in range(1, 41):
-        scheduler2.step(epoch)
-        epoch_lr.append(scheduler2.get_last_lr())
+    for epoch in range(epochs):
+        scheduler.optimizer.step()
+        scheduler.step()
+        default_lr.append(scheduler.get_last_lr())
 
     is_decreasing = [
         default_lr[i] < default_lr[i - 1] for i in range(1, len(default_lr))
     ]
 
-    torch.testing.assert_allclose(default_lr, epoch_lr)
     assert not all(is_decreasing[:9])
     assert all(is_decreasing[9:])
+    assert start_lr == default_lr[0][0]
+    assert last_lr == default_lr[-1][0]
