599c5104bf8f7665160df6bbe5cc2024052be345
joaopfonseca <jpmrfonseca@gmail.com>
2023-01-05 02:26:57 +0000

    TEST add tests for ContinuousCategoricalDatasets summary (#18)

--- mlresearch/datasets/tests/test_datasets.py
+++ mlresearch/datasets/tests/test_datasets.py
@@ -6,13 +6,17 @@ import numpy as np
 import pandas as pd
 
 from ..base import Datasets, FETCH_URLS
+from .._multiclass import ContinuousCategoricalDatasets
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
 MIN_CLASS = 20
 MAJ_CLASS = 100
 N_FEATURES = 4
-X = pd.DataFrame(np.random.random((30 + MIN_CLASS + MAJ_CLASS, N_FEATURES)))
+X = pd.DataFrame(
+    np.random.random((30 + MIN_CLASS + MAJ_CLASS, N_FEATURES)),
+    columns=[str(i) for i in range(N_FEATURES)],
+)
 y = pd.Series(
     [
         *[0 for i in range(MIN_CLASS)],
@@ -65,3 +69,48 @@ def test_imbalance_datasets():
     assert descr["Imbalance Ratio"].astype(int).tolist() == [
         i for i in irs if i >= base_ir
     ]
+
+
+def test_categorical_summary():
+    """Test if modification in summarize_datasets for ContinuousCategoricalDatasets
+    is working."""
+    base_ir = MAJ_CLASS / MIN_CLASS
+    irs = [1, 5, 10, 100]
+    exp_irs = [i for i in irs if i > base_ir]
+
+    datasets = ContinuousCategoricalDatasets()
+    datasets.content_ = [("test", pd.concat([X, y], axis=1))]
+
+    # Set up mixed type datasets
+    for n_cat in range(1, N_FEATURES + 1):
+        cat_columns = {
+            col: f"cat_{i}"
+            for i, col in enumerate(np.random.choice(X.columns, n_cat, replace=False))
+        }
+        name = f"test_{n_cat}"
+        data = pd.concat([X.rename(columns=cat_columns), y], axis=1)
+        data.loc[:, cat_columns.values()] = (
+            data[cat_columns.values()].round().astype(int)
+        )
+        datasets.content_.append((name, data))
+
+    # Imbalance datasets
+    for ir in irs:
+        datasets.imbalance_datasets(ir)
+
+    descr = datasets.summarize_datasets()
+
+    # summarize_datasets
+    assert descr.shape == ((len(exp_irs) + 1) * (N_FEATURES + 1), 9)
+    assert (descr["Features"] == N_FEATURES).all()
+    assert descr["Dataset name"].tolist() == list(dict(datasets.content_).keys())
+    assert (descr["Metric"] + descr["Non-Metric"] == descr["Features"]).all()
+
+    exp_cat = (
+        descr["Dataset name"]
+        .apply(lambda x: x.split("_")[-1].split(" ")[0])
+        .replace({"test": 0})
+        .astype(int)
+    )
+    true_cat = descr["Non-Metric"]
+    assert (exp_cat == true_cat).all()
